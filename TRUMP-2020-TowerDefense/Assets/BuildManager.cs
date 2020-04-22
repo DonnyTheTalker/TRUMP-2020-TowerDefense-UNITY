aefea12741aebbd5b5d838b8cf913482b1ee8736
DonnyTheTalker <maks.egorov.55@mail.ru>
2020-04-22 22:09:26 +0300

    Building Mechanics implemented

---
+++ TRUMP-2020-TowerDefense/Assets/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager Instance; 
+
+    void Awake()
+    {
+        Instance = this;
+    } 
+
+    private GameObject _turretToBuild;
+    public GameObject StandartTurretPrefab;
+
+    void Start()
+    {
+        _turretToBuild = StandartTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return _turretToBuild;
+    }
+
+}
---
+++ TRUMP-2020-TowerDefense/Assets/Scripts/Node.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine; 
+
+public class Node : MonoBehaviour
+{
+
+    public Color HoverColor;
+
+    private GameObject _turret;
+
+    private Renderer _renderer;
+    private Color _startColor;
+
+    public Vector3 PositionOffset;
+
+    void Start()
+    {
+
+        _renderer = GetComponent<Renderer>();
+        _startColor = _renderer.material.color;
+
+    }
+
+    void OnMouseEnter()
+    {
+        _renderer.material.color = HoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        _renderer.material.color = _startColor;
+    }
+
+    void OnMouseDown()
+    {
+        if (_turret != null) {
+
+            Debug.Log("Already built");
+
+        } else {
+
+            GameObject turretToBuild = BuildManager.Instance.GetTurretToBuild();
+            _turret = (GameObject)Instantiate(turretToBuild, transform.position + PositionOffset, transform.rotation);
+
+
+        }
+    }
+}
